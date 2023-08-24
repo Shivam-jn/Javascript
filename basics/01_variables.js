a4fd71a4bdb8b7cb195df6bd2ce7dbc1865cbdf3
Shivam-jn <107925674+Shivam-jn@users.noreply.github.com>
2023-08-24 10:52:17 +0000

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,14 @@
+const name = "shivam";
+let age = "20";
+var height = "5'8";
+weight = "58";
+
+let profession;
+
+/*
+prefer not to use var because of 
+its issue with block scope and function scope
+*/
+
+
+console.table([name,age,height,weight,profession]);
